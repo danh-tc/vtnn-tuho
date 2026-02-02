f98c35b2e7b1397b2ad07eb830f1fcb172288371
DANH Tran Cong <trancongdanh99@gmail.com>
2026-02-02 23:13:33 +0700

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
