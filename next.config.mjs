0c1977031fa788346a88df81b10356addeb59d97
DANH Tran Cong <trancongdanh99@gmail.com>
2026-02-02 23:06:51 +0700

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   output: "export",
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
