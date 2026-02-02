d4bbeecc02d3e81bb09afb4e0c886fe06a8fb222
DANH Tran Cong <trancongdanh99@gmail.com>
2026-02-02 23:01:32 +0700

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = "vtnn-tuho";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+  basePath: isProd ? `/${String(repoName)}` : "",
+  assetPrefix: isProd ? `/${String(repoName)}/` : "",
 };
 
 export default nextConfig;
