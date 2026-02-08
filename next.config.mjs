b0f8215f1522eb3184d02952efb75abf958eedab
DANH Tran Cong <trancongdanh99@gmail.com>
2026-02-08 12:40:08 +0700

    Remove export config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ const repoName = "vtnn-tuho";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}` : "",
   images: {
