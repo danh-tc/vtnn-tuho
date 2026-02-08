c70446465944b5110851ee8ea3775cc0bcd5aa63
DANH Tran Cong <trancongdanh99@gmail.com>
2026-02-08 12:52:08 +0700

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-const repoName = "vtnn-tuho";
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : "",
   },
 };
 
